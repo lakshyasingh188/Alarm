8b104d7ab940dd13f3148cb92d5557f694d89ef4
Lakshya singh <lakshyasinghe.c.e@gmail.com>
2026-03-08 17:53:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ const SUPABASE_URL = "https://sozdibpjfbjypzndoeyz.supabase.co"
 
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvemRpYnBqZmJqeXB6bmRvZXl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NDY5NjcsImV4cCI6MjA4ODUyMjk2N30.Vy9cFXM99ZiyB0OuhIEfeiSr6ab1boePlDSFXJKTbM4"
 
-const supabaseClient = window.supabase.createClient(
+const supabase = window.supabase.createClient(
 SUPABASE_URL,
 SUPABASE_KEY
 )
 
 async function signup(){
 
-const email = document.getElementById("email").value
-const password = document.getElementById("password").value
+const email=document.getElementById("email").value
+const password=document.getElementById("password").value
 
-const { data, error } = await supabaseClient.auth.signUp({
-email: email,
-password: password
+const {data,error}=await supabase.auth.signUp({
+email:email,
+password:password
 })
 
 if(error){
@@ -24,29 +24,6 @@ alert(error.message)
 }else{
 
 alert("Signup successful")
-window.location="index.html"
-
-}
-
-}
-
-async function login(){
-
-const email = document.getElementById("email").value
-const password = document.getElementById("password").value
-
-const { data, error } = await supabaseClient.auth.signInWithPassword({
-email: email,
-password: password
-})
-
-if(error){
-
-alert(error.message)
-
-}else{
-
-window.location="dashboard.html"
 
 }
 

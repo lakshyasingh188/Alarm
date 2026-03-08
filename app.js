49f938eec82f932d3213cd02d45bc0f1d7b4a4ba
Lakshya singh <lakshyasinghe.c.e@gmail.com>
2026-03-08 17:46:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const SUPABASE_URL = "https://sozdibpjfbjypzndoeyz.supabase.co"
 
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvemRpYnBqZmJqeXB6bmRvZXl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NDY5NjcsImV4cCI6MjA4ODUyMjk2N30.Vy9cFXM99ZiyB0OuhIEfeiSr6ab1boePlDSFXJKTbM4"
+const SUPABASE_KEY = "YOUR_ANON_KEY"
 
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
 SUPABASE_URL,
 SUPABASE_KEY
 )
 
 async function signup(){
 
-const email=document.getElementById("email").value
-const password=document.getElementById("password").value
+const email = document.getElementById("email").value
+const password = document.getElementById("password").value
 
-const {data,error}=await supabase.auth.signUp({
-
-email:email,
-password:password
-
+const { data, error } = await supabaseClient.auth.signUp({
+email: email,
+password: password
 })
 
 if(error){
@@ -26,7 +24,6 @@ alert(error.message)
 }else{
 
 alert("Signup successful")
-
 window.location="index.html"
 
 }
@@ -35,14 +32,12 @@ window.location="index.html"
 
 async function login(){
 
-const email=document.getElementById("email").value
-const password=document.getElementById("password").value
+const email = document.getElementById("email").value
+const password = document.getElementById("password").value
 
-const {data,error}=await supabase.auth.signInWithPassword({
-
-email:email,
-password:password
-
+const { data, error } = await supabaseClient.auth.signInWithPassword({
+email: email,
+password: password
 })
 
 if(error){

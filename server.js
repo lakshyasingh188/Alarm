319bf0c4cefa416ff7b7e5060569c6192bcbc8d9
Lakshya singh <lakshyasinghe.c.e@gmail.com>
2026-03-08 14:07:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,21 @@
-require("dotenv").config()
+const express=require("express")
 
-const express = require("express")
-const supabase = require("./supabase")
+const supabase=require("./supabase")
 
-const app = express()
+const app=express()
 
 app.use(express.json())
 
-app.post("/schedule", async (req,res)=>{
+app.post("/schedule",async(req,res)=>{
 
-const {phone,time,description} = req.body
+const {phone,time,description}=req.body
 
-const {data,error} = await supabase
+await supabase
 .from("calls")
 .insert([{phone,time,description}])
 
-if(error) return res.send(error)
-
-res.send("Call Scheduled")
+res.json({status:"scheduled"})
 
 })
 
-app.get("/",(req,res)=>{
-res.send("Voice Call API Running")
-})
-
-app.listen(3000,()=>{
-console.log("Server running")
-})
+app.listen(3000)

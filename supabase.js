7d39edc1957837ca4bc954cc877657abbe5e4ae1
Lakshya singh <lakshyasinghe.c.e@gmail.com>
2026-03-08 17:32:08 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
-const { createClient } = require("@supabase/supabase-js")
+const SUPABASE_URL = "https://sozdibpjfbjypzndoeyz.supabase.co"
 
-const supabase = createClient(
-process.env.SUPABASE_URL,
-process.env.SUPABASE_KEY
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvemRpYnBqZmJqeXB6bmRvZXl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NDY5NjcsImV4cCI6MjA4ODUyMjk2N30.Vy9cFXM99ZiyB0OuhIEfeiSr6ab1boePlDSFXJKTbM4"
+
+const supabase = window.supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
 )
-
-module.exports = supabase
